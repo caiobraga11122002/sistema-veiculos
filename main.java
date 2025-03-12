af0abc4c67befca677d5d4f38ce46d997098024c
Caio Vinicius Braga <C27594@hmaterdei.com.br>
2025-03-12 20:21:25 -0300

    primeiro commit

---
+++ main.java
@@ -0,0 +1,18 @@
+{
+    "workbench.colorTheme": "Dracula Theme",
+    "workbench.editor.empty.hint": "hidden",
+    "liveServer.settings.donotShowInfoMsg": true,
+    "redhat.telemetry.enabled": true,
+    "files.associations": {
+        "*.java": "java"
+    },
+    "[java]": {
+
+        "editor.suggest.snippetsPreventQuickSuggestions": false
+
+        for(){
+            
+        }
+        
+    }
+}
